1830c7b5ea579cd8a5472160641393b40f63d5c4
guerler <aysam.guerler@gmail.com>
2024-08-11 01:48:00 +0300

    Add server configuration file

--- charts/vite.config.js
+++ charts/vite.config.js
@@ -2,6 +2,17 @@ import { defineConfig } from "vite";
 import tailwindcss from "tailwindcss";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
+import serverConfig from "./server.config";
+
+// determine server route
+let GALAXY_API = "";
+if (process.env.GALAXY_API) {
+    GALAXY_API = process.env.GALAXY_API;
+} else if (serverConfig.GALAXY_API) {
+    GALAXY_API = serverConfig.GALAXY_API;
+} else {
+    console.warn("GALAXY_API not available. Please provide as environment variable or specify in 'server.config'.");
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,7 +35,7 @@ export default defineConfig({
     server: {
         proxy: {
             "/api": {
-                target: "http://127.0.0.1:8081/api/",
+                target: GALAXY_API,
                 changeOrigin: true,
                 rewrite: (path) => path.replace(/^\/api/, ""),
             },

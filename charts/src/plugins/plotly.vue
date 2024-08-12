9565a78d5b2cf4891623c6d5b3188b53b602d1e4
guerler <aysam.guerler@gmail.com>
2024-08-12 08:04:03 +0300

    Prep adding plotly

--- charts/src/plugins/plotly.vue
+++ charts/src/plugins/plotly.vue
@@ -1,21 +1,41 @@
 <script setup>
-import VuePlotly from 'vue-plotly';
+import Plotly from "plotly.js-dist";
+import { onMounted, ref } from "vue";
 
 const props = defineProps({
     datasetUrl: String,
     settings: Object,
 });
 
-const plotData = [{
-      x: [1,2,3,4],
-      y: [10,15,13,17],
-      type:"scatter"
-    }];
+const viewport = ref(null);
+const plotData = [
+    {
+        x: [1, 2, 3, 4],
+        y: [10, 15, 13, 17],
+        type: "scatter",
+    },
+];
 const layout = {
-      title: "My graph"
-    };
+    title: "My graph",
+};
+
+onMounted(() => {
+    //Plotly.newPlot(viewport.value, [{ y: [1, 2, 3] }])
+    var data = [
+        {
+            z: [
+                [1, 20, 30],
+                [20, 1, 60],
+                [30, 60, 1],
+            ],
+            type: "heatmap",
+        },
+    ];
+
+    Plotly.newPlot(viewport.value, data);
+});
 </script>
 
 <template>
-    <VuePlotly :data="plotData" :layout="layout" :display-mode-bar="false" />
+    <div ref="viewport" />
 </template>

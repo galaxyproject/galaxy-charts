a9fac7b252e7e377d5bb6ff40e404c9ad868239f
guerler <aysam.guerler@gmail.com>
2024-08-17 18:02:15 +0300

    Move to root directory

--- plain/script.js
+++
@@ -1,4 +0,0 @@
-var element = document.getElementById("app");
-var div = document.createElement('div');
-div.innerHTML = JSON.stringify(JSON.parse(element.getAttribute("data-incoming")));
-document.body.appendChild(div);

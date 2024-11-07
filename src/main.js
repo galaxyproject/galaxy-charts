e10ee5f03536c10ce8bf025231daca34021d04d5
guerler <aysam.guerler@gmail.com>
2024-11-07 14:59:37 +0300

    Run the xml parser in main, and parse result through container

--- src/main.js
+++ src/main.js
@@ -1,27 +1,33 @@
 import { createApp, h } from "vue";
 import App from "./App.vue";
+import { parseXML } from "./utilities/parseXML";
 
-const dataIncoming = {
-    visualization_config: {
-        dataset_url: null,
-        dataset_id: "12e4b4feedfe9f3f",
-        settings: {
-            setting_text: "My Test Setting",
-            setting_boolean: true,
-            setting_conditional: {
-                case_false: "something else",
-                test_condition: "false",
+async function main() {
+    // Build the incoming data object
+    const dataIncoming = {
+        visualization_config: {
+            dataset_url: null,
+            dataset_id: "12e4b4feedfe9f3f",
+            settings: {
+                setting_text: "My Test Setting",
+                setting_boolean: true,
+                setting_conditional: {
+                    case_false: "something else",
+                    test_condition: "false",
+                },
             },
         },
-    },
-};
+        visualization_plugin: await parseXML("galaxy-charts.xml"),
+    };
 
-const xml = "galaxy-charts.xml";
+    // Attach config to the data-incoming attribute
+    const appElement = document.querySelector("#app");
+    appElement.setAttribute("data-incoming", JSON.stringify(dataIncoming));
 
-// Attach config to the data-incoming attribute
-const appElement = document.querySelector("#app");
-appElement.setAttribute("data-incoming", JSON.stringify(dataIncoming));
+    // Mount the app
+    createApp({
+        render: () => h(App, { credentials: process.env.credentials }),
+    }).mount("#app");
+}
 
-createApp({
-    render: () => h(App, { credentials: process.env.credentials, xml: xml }),
-}).mount("#app");
+main();

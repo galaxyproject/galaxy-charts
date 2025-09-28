c8e41703df47f07d978975d5c54c03a103ebf41b
guerler <aysam.guerler@gmail.com>
2025-09-28 10:24:27 +0300

    Update data json store, response can only be read once

--- src/store/dataJsonStore.ts
+++ src/store/dataJsonStore.ts
@@ -1,48 +1,39 @@
 import { ref } from "vue";
 import type { InputSelectOptionType } from "@/types";
 
-type ValueType = {
-    id: string;
-    name?: string;
-};
+type ValueType = { id: string; name?: string };
 
-const dataJsonCache = ref<Record<string, Promise<Response>>>({});
+const dataJsonCache = ref<Record<string, Promise<Array<InputSelectOptionType>>>>({});
 
 export function useDataJsonStore() {
-    async function parseDataJson(url: string, promise: Promise<Response>): Promise<Array<InputSelectOptionType>> {
+    async function fetchDataJson(url: string): Promise<Array<InputSelectOptionType>> {
         try {
-            const response = await promise;
-            if (response.ok) {
-                const result: ValueType[] = await response.json();
-                return result.map((entry: ValueType) => ({
-                    label: entry.name || entry.id,
-                    value: { ...entry },
-                }));
-            } else {
+            const response = await fetch(url);
+            if (!response.ok) {
                 console.error("[charts] Failed to request data json.", response.status);
-                delete dataJsonCache.value[url];
                 return [];
             }
+            const result: ValueType[] = await response.json();
+            return result.map((entry) => ({
+                label: entry.name || entry.id,
+                value: { ...entry },
+            }));
         } catch (err) {
             console.debug("[charts] Failed to request data json.", err);
-            delete dataJsonCache.value[url];
             return [];
         }
     }
 
     async function getDataJson(url: string): Promise<Array<InputSelectOptionType>> {
         if (!dataJsonCache.value[url]) {
-            dataJsonCache.value[url] = fetch(url);
+            dataJsonCache.value[url] = fetchDataJson(url);
         }
-        return await parseDataJson(url, dataJsonCache.value[url]);
+        return await dataJsonCache.value[url];
     }
 
     function resetDataJson() {
         dataJsonCache.value = {};
     }
 
-    return {
-        getDataJson,
-        resetDataJson,
-    };
+    return { getDataJson, resetDataJson };
 }
